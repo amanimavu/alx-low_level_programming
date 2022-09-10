ffceed0e3e60450df0313e3cbc43bb9b31817928
amanimavu <amanimavu@gmail.com>
2022-09-10 12:57:46 -0700

    create a script that displays text using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - is the entry point of the program
+ * Description: this program display a statement
+ * using the printf method
+ * Return: reurns a value of 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
