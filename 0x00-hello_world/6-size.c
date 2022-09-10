3f4c46ab29b40106c401ae67aaef3f25c64588fe
amanimavu <amanimavu@gmail.com>
2022-09-10 13:55:14 -0700

    add a newline to every statement

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,9 @@
  */
 int main(void)
 {
-	printf("Size of a char: %lu", sizeof(char));
-	printf("Size of an int: %lu", sizeof(int));
-	printf("Size of long int: %lu", sizeof(long));
-	printf("Size of a long long int: %lu", sizeof(long long));
-	printf("Size of a float: %lu", sizeof(float));
+	printf("Size of a char: %lu\n", sizeof(char));
+	printf("Size of an int: %lu\n", sizeof(int));
+	printf("Size of long int: %lu\n", sizeof(long));
+	printf("Size of a long long int: %lu\n", sizeof(long long));
+	printf("Size of a float: %lu\n", sizeof(float));
 }
