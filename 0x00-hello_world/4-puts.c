f4fe92db23dc7795b7faa334674c4be8161798a8
amanimavu <amanimavu@gmail.com>
2022-09-10 12:10:44 -0700

    add description and documentation of the file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - entry point of the program
+ * Description: what this function does is that
+ * it displays a statement using the puts method
+ * Return: 0 is what is returned from main
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
