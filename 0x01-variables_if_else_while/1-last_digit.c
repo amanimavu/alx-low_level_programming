3bea6d605ba73a4f5f1086c0bf050adc98f4a50c
Amani <amanimavu@gmail.com>
2022-09-12 11:02:35 -0700

    generate random numbers and test the last digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ * Description: program that genarates random number
+ * and checks last digit of the number is >5, 0 or <6
+ *
+ * Return: always return 0(SUCCESS)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int last_digit = n % 10;
+
+	if (last_digit == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
+	}
+else
+	{
+		if (last_digit > 5)
+			printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
+		else
+			printf("Last digit of %d is %d and is less than 6\n", n, last_digit);
+	}
+
+	return (0);
+}
