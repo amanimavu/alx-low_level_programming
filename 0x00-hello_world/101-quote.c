bc367e37918f5be3603b0944e5525e39f6a591cc
Amani <amanimavu@gmail.com>
2022-09-12 07:27:28 -0700

    program that displays text on standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ * Description: program displays text using
+ * fprintf on standard error
+ *
+ * Return: 0 is always returned
+ */
+int main(void)
+{
+	char str1[25] = "and that piece of art is";
+	char str2[25] = "Dora Korpar, 2015-10-19\n";
+
+	fprintf(stderr, "%s useful\" - %s", str1, str2);
+	return (0);
+}
