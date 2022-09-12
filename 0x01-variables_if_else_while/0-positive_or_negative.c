92e7a8864956d16c881dd078d180ae13db7df8e4
Amani <amanimavu@gmail.com>
2022-09-12 09:03:04 -0700

    test a random generated number is +ve, -ve or neutral

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point of the program
+ * Description: the program assigns a random
+ * number to a variable and checks whether
+ * it is positive, negative or neutral
+ *
+ * Return: Always return a success value(0)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n < 0)
+		printf("%d is negative\n", n);
+	else
+		printf("%d is zero\n", n);
+	return (0);
+
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,3 @@
+The directory in which this file is in contains files
+that allows programmer exercise controlled logic in c,
+using variable and loops
