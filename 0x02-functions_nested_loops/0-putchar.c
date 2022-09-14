5a8ea23a2da7cc6877af0abc20951dc4d9396aee
Amani <amanimavu@gmail.com>
2022-09-14 15:55:03 -0700

    included the main function in 0-putchar file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,27 @@
 #include "main.h"
 /**
-  * print_text - displays single character of text
-  * @string: is an array of character (string)
-  * Description: loops through a string and extracts
-  * characters, then displays them
+  * main - entry point of the program
+  * Dscription: invoking print_text function
   *
-  * Return: Doesn't return any value
+  *Return: always returns a zero
+  */
+
+int main(void)
+{
+	char word[50] = "_putchar.";
+
+	print_text(word);
+	return (0);
+}
+
+
+/**
+  * print_text - prints text
+  * Description: loops though a string and uses
+  * the _putchar user defines method to print character
+  * @string: character array parameter
+  *
+  * Return: always returns a 0
   */
 
 void print_text(char string[])
