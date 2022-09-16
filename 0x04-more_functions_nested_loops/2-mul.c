6690e06a6c11d2403db45a517854e9fe2d1b4208
Amani <amanimavu@gmail.com>
2022-09-16 15:20:34 -0700

    evaluate the product of two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+/**
+ * mul - evaluates the product of 2 integers
+ * @a: first integer
+ * @b: second integer
+ * Return: product of the 2 integera
+ */
+
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
