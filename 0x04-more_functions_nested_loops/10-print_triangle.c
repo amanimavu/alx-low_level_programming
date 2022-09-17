c3fa82771a364463e0a1f2dc4b4754afd59410ff
Amani <amanimavu@gmail.com>
2022-09-16 19:58:34 -0700

    consider cases where size < 1

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,15 +12,22 @@ void print_triangle(int size)
 	int i;
 	int j;
 
-	for (i = 0; i < size; i++)
+	if (size > 0)
 	{
-		for (j = 1; j <= size; j++)
+		for (i = 0; i < size; i++)
 		{
-			if (j >= (size - i))
-				_putchar('#');
-			else
-				_putchar(' ');
+			for (j = 1; j <= size; j++)
+			{
+				if (j >= (size - i))
+					_putchar('#');
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
+	}
+	else
+	{
 		_putchar('\n');
 	}
 }
