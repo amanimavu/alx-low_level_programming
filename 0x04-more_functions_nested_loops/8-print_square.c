920e25e7ac95e673a2e0eeaf8969c4e78856c54b
Amani <amanimavu@gmail.com>
2022-09-16 18:19:33 -0700

    print square of side size using #

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_square - print a square of sides size x size
+ * @size: length and height of square
+ * Description: uses # to print the square using nested for loop
+ *
+ * Return: No return value
+ */
+
+void print_square(int size)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
