0b450993ddb76085830d874047aa96a2eebb2584
Amani <amanimavu@gmail.com>
2022-09-19 15:56:55 -0700

    updating value of a variable using pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - update value to point to 98
+ * @n: pointer of type int
+ * Return: no return value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Description of files in the current working directory
+Going through the topic of arrays, pointers, strings and data structure
