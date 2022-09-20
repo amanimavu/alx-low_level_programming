1415fddcdd65b5070635ec25860eed030f8d3cbd
Amani <amanimavu@gmail.com>
2022-09-20 10:54:44 -0700

    corrected my code

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -22,12 +22,6 @@ void print_array(int *a, int n)
 		}
 	else
 	{
-		for (i = 0; i > n; i--)
-		{
-			if (i !=( n +1))
-				printf("%d, ", *(a + i));
-			else
-				printf("%d\n", *(a + i));
-		}
+		printf("\n");
 	}
 }
