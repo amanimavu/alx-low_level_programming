3ab90ad10fdc6127d65024b2deb77f9aca11b772
Amani <amanimavu@gmail.com>
2022-09-20 10:29:38 -0700

    made some few corrections

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -25,9 +25,9 @@ void print_array(int *a, int n)
 		for (i = 0; i >= n; i--)
 		{
 			if (i != n)
-				printf("%d, ", *(a + i), i);
+				printf("%d, ", *(a + i));
 			else
-				printf("%d\n", *(a + i), i);
+				printf("%d\n", *(a + i));
 		}
 	}
 }
