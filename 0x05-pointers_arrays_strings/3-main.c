17349ca72320acc1938d0404349a65580061e498
Amani <amanimavu@gmail.com>
2022-09-19 19:09:09 -0700

    creating a puts method

---
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char *str1, *str2, *str3;
+
+	str1 = "My first strlen!";
+	str2 = "";
+	str3 = "Holberton!";
+
+	_puts(str1);
+	_puts(str2);
+	_puts(str3);
+	return (0);
+}
---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - print a string followed by newline
+ * @str: string array
+ * Return: no return value
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
