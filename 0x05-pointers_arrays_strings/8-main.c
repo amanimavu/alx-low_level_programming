5d8583f4f3bc1807ffe74abdac7c1da2b5aefe6e
Amani <amanimavu@gmail.com>
2022-09-20 10:25:46 -0700

    considered negative input

--- 0x05-pointers_arrays_strings/8-main.c
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -10,12 +10,16 @@ int main(void)
 {
 	int array[5];
 
+	int array1[1] = {
+		980
+	};
 	array[0] = 98;
 	array[1] = 402;
 	array[2] = -198;
 	array[3] = 298;
 	array[4] = -1024;
 
+	print_array(array1, -1);
 	print_array(array, 5);
 	return (0);
 }
