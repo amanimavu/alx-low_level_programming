2580a7ee6ddbc6232b8c3a59ab76b3252a3d8086
Amani <amanimavu@gmail.com>
2022-09-21 10:26:40 -0700

    concatenate n bytes of string from src to dest string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strncat - concatenate a n bytes of string from source
+ * to destination string
+ * @dest: destination string to which characters are to be added to
+ * @src: source string from which characters are to be extracted
+ * n: number of bytes to extract
+ *
+ * Return: destination string
+ */
+
+char *_strcat(char *dest, char *src, int n)
+{
+	int i;
+	int len_src;
+	int len_dest;
+
+	len_src = strlen(src);
+	len_dest = strlen(dest);
+	for (i = 0; i <= (n - 1); i++)
+	{
+		*(dest + (i + len_dest)) = *(src + i);
+	}
+
+	return (dest);
+}
