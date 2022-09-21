159bafb3215c7bd63503b1e11a053478531b48c2
Amani <amanimavu@gmail.com>
2022-09-21 12:29:16 -0700

    copy n bytes of characters from source string to destination string

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies n bytes of characters from src to dest string
+ * @dest: destination string
+ * @src: source string
+ * @n: bytes to be copied
+ *
+ * Return: modified destination string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+	int len_src;
+	int len_dest;
+
+	len_src = strlen(src);
+	len_dest = strlen(dest);
+	n = n > len_src ? len_src : n - 1;
+	for (i = 0; i <= n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
