5756cc6d6ffd8d38a2efa79e9aac440fdba57c2a
Amani <amanimavu@gmail.com>
2022-09-21 17:00:12 -0700

    made corrections on my code

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include  <string.h>
 
 /**
  * _strcmp - compares two strings
@@ -18,5 +17,5 @@ int _strcmp(char *s1, char *s2)
 		s1++;
 		s2++;
 	}
-	return (*s1 - *s2)
+	return (*s1 - *s2);
 }
