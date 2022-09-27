e657a545f9dc3f38e47b8f684b123c1de1afd103
Amani <amanimavu@gmail.com>
2022-09-27 08:56:02 -0700

    a function that seacher for a word in another string

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * _strstr - checks if a substring(more than 1 char) exists in a string
+ * @haystack: Target string in which we do the searching
+ * @needle: substring that we are looking for in haystack
+ * Return: pointer to the begining of the located string or
+ * NULL if substring not found
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i;
+	int j;
+	int len_j;
+
+	len_j = strlen(needle);
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[0])
+		{
+			for (j = 1; j < len_j;)
+			{
+				i++;
+				if (haystack[i] == needle[j])
+					j++;
+				else
+					break;
+			}
+			if (j == len_j)
+			{
+				haystack += (i - j + 1);
+				return (haystack);
+			}
+		}
+	}
+	return (NULL);
+}
