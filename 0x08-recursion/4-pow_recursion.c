ed7c4f43569cc8ea55d318eaa90bf8f9c9a88b95
Amani <amanimavu@gmail.com>
2022-09-29 05:50:22 -0700

    finding the power of number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - evaluate the power of a number x(x ^ y)
+ * @x: the base of the power
+ * @y: the index of the power
+ * Return: power of a number
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y > 0)
+		return (x * _pow_recursion(x, y - 1));
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+}
