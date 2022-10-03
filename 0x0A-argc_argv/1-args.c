c5379c7ee922114194668519d8970198f5b8267b
Amani <amanimavu@gmail.com>
2022-10-02 21:30:50 -0700

    prints the number of command line arguments excluding the pragram name

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments pased into it
+ * @argc: count of the command line arguments
+ * @argv: 1-D array of command line arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char * argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
