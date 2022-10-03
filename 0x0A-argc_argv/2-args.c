d60890aa9572aaacdc5972e9cc4837432f46106e
Amani <amanimavu@gmail.com>
2022-10-02 22:08:31 -0700

    print every command line argument

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints every command line argument, each on a newline
+ * @argc: command line argument count
+ * @argv: a 1-D array of command line arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int index;
+
+	for(index = 0; index < argc; index++)
+	{
+		printf("%s\n", argv[index]);
+	}
+	return (0);
+}
