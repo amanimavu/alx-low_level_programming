a41438f0d909d2ac56ceee66e8b5cfa0712a428d
Amani <amanimavu@gmail.com>
2022-10-04 08:38:29 -0700

    consider when size is 0 and if malloc fails

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,15 @@ char *create_array(unsigned int size, char c)
 	char *arr;
 	unsigned int i;
 
+	if (size == 0)
+	{
+		return (NULL);
+	}
 	arr = malloc(sizeof(char) * size);
+	if (!arr)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
