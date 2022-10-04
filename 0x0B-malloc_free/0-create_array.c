63839067ab35b74c8ab148bbc84f083c1eed7ea2
Amani <amanimavu@gmail.com>
2022-10-04 08:12:05 -0700

    create a buffer and initialize it

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+
+/**
+ * create_array - creates an arrary and initialize it with a specific char
+ * @size: size of the created array
+ * @c: the specific char we are to initialize the array with
+ * Return: pointer to characters
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	int i;
+
+	arr = malloc(sizeof(char) * size);
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	return (arr);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+Getting practical with using malloc for dynamic allocation of memory
