0f03a6009df8b88a087b94f67e39a12eacbd56d2
Amani <amanimavu@gmail.com>
2022-10-05 14:20:30 -0700

    create a 2-D array and initialize it to 0

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+
+/**
+ * alloc_grid - creates a 2-D array with entries initialized to 0
+ * @width: columns of the 2-D array
+ * @height: rows of the 2-D array
+ * Return: pointer to pointers of integers
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int **arr;
+	int i, j;
+
+	if (height <= 0 || width <= 0)
+	{
+		return (NULL);
+	}
+	arr = (int **)malloc(height * sizeof(int *));
+	if (!arr)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = (int *)malloc(width * sizeof(int));
+		for (j = 0; j < width; j++)
+		{
+			arr[i][j] = 0;
+		}
+	}
+	return (arr);
+}
