1fa4e3af6fb0d291fe4d8fe8d01ab4badef65e9e
Amani <amanimavu@gmail.com>
2022-10-05 15:24:08 -0700

    add free on multiple places

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = (int *)malloc(width * sizeof(int));
-		if (!arr)
+		if (!arr[i])
 		{
 			free(arr);
 			return (NULL);
