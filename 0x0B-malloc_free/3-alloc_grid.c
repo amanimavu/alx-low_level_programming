0a13d8125fc831c9ed62c28f8343315ccf1d70a2
Amani <amanimavu@gmail.com>
2022-10-05 14:46:46 -0700

    free evrything when any malloc fails

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,6 +25,11 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = (int *)malloc(width * sizeof(int));
+		if (!arr[i])
+		{
+			free(arr[i]);
+			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 		{
 			arr[i][j] = 0;
