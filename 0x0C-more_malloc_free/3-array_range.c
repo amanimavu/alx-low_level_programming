add6e476c7d532b36f468751f135a285ce46535f
Amani <amanimavu@gmail.com>
2022-10-07 07:21:07 -0700

    consider the constraints of the program

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,8 +12,12 @@ int *array_range(int min, int max)
 	int size, i;
 	int *ptr;
 
+	if (min > max)
+		return (NULL);
 	size = max - min + 1;
 	ptr = malloc(sizeof(int) * size);
+	if (!ptr)
+		return (NULL);
 	for (i = min; i <= max; i++)
 	{
 		*(ptr + (i - min)) = i;
