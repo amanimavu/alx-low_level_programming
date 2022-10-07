ad0d13ac0133eb44fc36478a6c6cbc56a370c467
Amani <amanimavu@gmail.com>
2022-10-07 14:40:12 -0700

    implement a realloc method

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+
+/**
+ * _realloc - reallocates memory block using malloc and free
+ * @ptr: pointer previously allocated
+ * @old_size: size previously allocated
+ * @new_size: size to be allocated
+ * Return: pointer to the allocated space with new_size
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+	if (ptr == NULL)
+	{
+		ptr = malloc(new_size);
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
+		return (NULL);
+	}
+	if (new_size == old_size)
+	{
+		return (ptr);
+	}
+	if (new_size > old_size)
+	{
+		new_ptr = malloc(new_size);
+		memcpy(new_ptr, ptr, old_size);
+		free(ptr);
+	}
+	if (new_size < old_size)
+	{
+		new_ptr = malloc(new_size);
+		memcpy(new_ptr, ptr, new_size);
+		free(ptr);
+	}
+	return (new_ptr);
+}
