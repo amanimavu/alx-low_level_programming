b26039851048a8cb79a00e22155ed2bc4e77ccf3
Amani <amanimavu@gmail.com>
2022-10-06 17:10:53 -0700

    Started on 0x0C-more_malloc_free directory
    - Create a README.md file
    - Create 0-malloc_checked.c file
    - File allocates memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory using malloc
+ * @b: memory size to be allocated
+ * Return: address of allocated memory or if malloc
+ * fail a program termination status of 98
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *memory;
+
+	memory = malloc(b);
+	if (memory != NULL)
+	{
+		return (memory);
+	}
+	else
+	{
+		return (98);
+	}
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,4 @@
+0-malloc_checked.c: contains funtions that allocates memory using malloc
+1-string_nconcat.c: contains a program that concatenates 2 strings
+2-calloc.c: contains a progrm that allocates memory for an array using malloc
+3-array_range.c: contains a program that creates an array
