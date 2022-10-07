5c72c3f926d54d80e27ddb3bf478e189505809c2
Amani <amanimavu@gmail.com>
2022-10-07 05:23:47 -0700

    write a program that allocates memory with value of 0

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - allocates a memory in space and initializes the
+ * memory with 0
+ * @nmemb: number of elements to be held in the memory
+ * @size: size of each element in bytes
+ *
+ * Return: pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+	unsigned int space, i;
+
+	space = nmemb * size;
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	ptr = malloc(space);
+	if (!ptr)
+	{
+		return (NULL);
+	}
+	memset(ptr, 0, nmemb);
+	return (ptr);
+}
