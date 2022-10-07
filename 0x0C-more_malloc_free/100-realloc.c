530577e64926f1e137c7f2bcca5d36146acd8166
Amani <amanimavu@gmail.com>
2022-10-07 14:47:11 -0700

    added the string library

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-
+#include <string.h>
 /**
  * _realloc - reallocates memory block using malloc and free
  * @ptr: pointer previously allocated
@@ -11,6 +11,7 @@
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *new_ptr;
+
 	if (ptr == NULL)
 	{
 		ptr = malloc(new_size);
