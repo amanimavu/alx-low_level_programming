58956a3cf90ad2ec85f2cfbb8a610714235127f1
Amani <amanimavu@gmail.com>
2022-10-09 08:53:44 -0700

    made used null on the 2nd if and added everything

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(width * sizeof(int));
-		if (!arr[i])
+		if (arr[i] == NULL)
 		{
 			free(arr);
 			return (NULL);
