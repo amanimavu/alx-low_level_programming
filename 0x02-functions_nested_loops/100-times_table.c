b465ef5fd65af6bafcb2bf8221e096e9f1bda2d4
Amani <amanimavu@gmail.com>
2022-10-10 07:16:37 -0700

    change null byte to to '0'

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -23,19 +23,19 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 				}
-				_putchar((i * j) + '\0');
+				_putchar((i * j) + '0');
 			}
 			if (i * j > 9 && (i * j) < 100)
 			{
 				_putchar(' ');
-				_putchar(((i * j) / 10) + '\0');
-				_putchar(((i * j) % 10) + '\0');
+				_putchar(((i * j) / 10) + '0');
+				_putchar(((i * j) % 10) + '0');
 			}
 			if (i * j > 99)
 			{
-				_putchar((((i * j) / 10) / 10) + '\0');
-				_putchar((((i * j) / 10) % 10) + '\0');
-				_putchar(((i * j) % 10) + '\0');
+				_putchar((((i * j) / 10) / 10) + '0');
+				_putchar((((i * j) / 10) % 10) + '0');
+				_putchar(((i * j) % 10) + '0');
 			}
 			if (j < n)
 			{
