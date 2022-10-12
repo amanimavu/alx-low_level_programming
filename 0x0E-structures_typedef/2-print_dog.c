104f20eed1f92d341138f521584d5645444c6c18
Amani <amanimavu@gmail.com>
2022-10-12 15:16:04 -0700

    corrected my condition for age

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -12,14 +12,28 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		if ((*d).name)
+		{
 			printf("Name: %s\n", d->name);
+		}
 		else
+		{
 			printf("Name: (nil)\n");
-		if ((*d).age)
+		}
+		if ((*d).age >= 0)
+		{
 			printf("Age: %f\n", d->age);
-		if ((*d).owner)
-			printf("Owner: %s\n", d->owner);
+		}
 		else
+		{
+			printf("Age: (nil)\n");
+		}
+		if ((*d).owner)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
 			printf("Owner: (nil)\n");
+		}
 	}
 }
