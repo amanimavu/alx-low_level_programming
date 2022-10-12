a9d389f8693fea971e80523e47131d9b283f78c7
Amani <amanimavu@gmail.com>
2022-10-12 16:32:44 -0700

    assigned copies to elements of new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,4 +1,6 @@
 #include "dog.h"
+#include <string.h>
+#include <stdlib.h>
 
 /**
  * new_dog - creates a new dog
@@ -13,13 +15,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t Dog;
 	dog_t *Doggie;
+	char *name_dest = malloc(sizeof(char) * (strlen(name) + 1));
+	char *owner_dest = malloc(sizeof(char) * (strlen(owner) + 1));
 
 	Doggie = &Dog;
+	strcpy(name_dest, name);
+	strcpy(owner_dest, owner);
 	if (Doggie)
 	{
-		Doggie->name = name;
+		Doggie->name = name_dest;
 		Doggie->age = age;
-		Doggie->owner = owner;
+		Doggie->owner = owner_dest;
 	}
 
 	return (Doggie);
