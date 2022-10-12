92bf15c1d5a5213c49ba158a8fd44cad7b836a93
Amani <amanimavu@gmail.com>
2022-10-12 01:51:40 -0700

    documented my structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - properties of a dog
+ * @name: char pointer to name of a dog
+ * @age: int pointer to age of the dog
+ * @owner: char pointer to name of the owner
+ */
+
 struct dog
 {
 	char *name;
