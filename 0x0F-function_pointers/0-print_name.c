f90575ddce8694f6e5c742682c8245879830ae1e
Amani <amanimavu@gmail.com>
2022-10-13 13:41:28 -0700

    check if name pointer is null

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 /**
  * print_name - prints a name specified by function pointer
  * @name: name to be displayed
@@ -8,5 +9,10 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-	f(name);
+	if (name)
+	{
+		f(name);
+		exit(EXIT_SUCCESS);
+	}
+	exit(EXIT_FAILURE);
 }
