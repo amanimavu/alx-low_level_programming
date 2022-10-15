5bbf92f42e057f4cf60034cf45e35ae316879661
Amani <amanimavu@gmail.com>
2022-10-15 04:47:04 -0700

    betty compliancy and exit status

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,7 +3,11 @@
 #include <stdio.h>
 
 /**
+ * main - entry point of the program
+ * @argc: argument count
+ * @argv: argument vector
  *
+ * Return: Always 0
  */
 
 int main(int argc, char *argv[])
@@ -18,7 +22,8 @@ int main(int argc, char *argv[])
 		num2 = atoi(argv[3]);
 		sign = argv[2];
 
-		if(((*argv[1] != '0' && num1) || *argv[1] == '0') && ((*argv[3] != '0' && num2) || *argv[3] == '0'))
+		if (((*argv[1] != '0' && num1) || *argv[1] == '0')
+		&& ((*argv[3] != '0' && num2) || *argv[3] == '0'))
 		{
 			operation = get_op_func(sign);
 			if (operation)
@@ -29,8 +34,14 @@ int main(int argc, char *argv[])
 			else
 			{
 				printf("Error\n");
+				exit(99);
 			}
 		}
+	}
+	else
+	{
+		printf("Error\n");
+		exit(98);
 	}
 	return (0);
 }
