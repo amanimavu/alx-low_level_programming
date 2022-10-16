7181f4109ce3cea9abe8b09938f11956a8b89f60
Amani <amanimavu@gmail.com>
2022-10-16 13:38:45 -0700

    change nill for nil

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -21,7 +21,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (string)
 			printf("%s", string);
 		else
-			printf("(nill)");
+			printf("(nil)");
 		if ((i != n - 1) && separator)
 			printf("%s", separator);
 	}
