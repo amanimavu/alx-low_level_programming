b2d936878b20a406dafeb1f678bf8093812f119e
Amani <amanimavu@gmail.com>
2022-10-15 17:56:32 -0700

    include the correct arguments to va_arg

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,6 +1,5 @@
 #include <stdarg.h>
 #include <stdio.h>
-
 /**
  * print_strings - prints strings given as arguments
  * @separator: string that separates the argument strings
@@ -12,17 +11,22 @@
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	unsigned int i;
-
 	va_list ap;
+
 	va_start(ap, n);
 	for (i = 0; i < n; i++)
 	{
-		if (va_arg(ap, int))
-			printf("%s", va_arg(ap, int));
+		if (va_arg(ap, char*))
+		{
+			printf("%s", va_arg(ap, char*));
+		}
 		else
+		{
 			printf("(nill)");
+		}
 		if ((i != n - 1) && separator)
 			printf("%s", separator);
 	}
+	va_end(ap);
 	printf("\n");
 }
