97e519c2bdc73e192e63e085edba879d9e8c4e7e
Amani <amanimavu@gmail.com>
2022-11-06 22:10:45 -0800

    Modified and added some if statements 1. correct the if for handling when filename is NULL 2. add an if when an error occured during read

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,6 +1,9 @@
 #include <fcntl.h>
 #include <unistd.h>
-#include <unistd.h>
+#include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <stdio.h>
 
 /**
  * read_textfile - reads a text file and prints it in the stardard output
@@ -15,16 +18,37 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	ssize_t bytes_read, bytes_written;
 	char *buf = malloc(sizeof(char) * letters);
-	mode_t mode = S_IRUSR | S_IRGRP | S_IROTH;
+	if (!buf)
+		return (0);
 
-	if (filename)
-		return (0)
-	fd = open(filename, O_RDONLY, mode);
+	/*return 0 when filename is NULL*/
+	if (!filename)
+		return (0);
+
+	/* open the file specified by the filename
+	 * return 0 if an error occured
+	 */
+	fd = open(filename, O_RDONLY, 0600);
+	if (fd == -1)
+		return (0);
+
+	/**
+	 * read the opened file specified by fd
+	 * return 0 if an erro occured
+	 * close the file descriptor
+	 */
 	bytes_read = read(fd, buf, letters);
 	if (bytes_read == -1)
 		return (0);
+	close(fd);
 
+	/**
+	 * write to buffer
+	 * return 0 if bytes written are not equal to bytes read
+	 * and if an error occured when writeing to stdout
+	 */
 	bytes_written = write(STDOUT_FILENO, buf, letters);
-	if (bytes_written != letters || bytes == -1)
+	if (letters != (size_t)bytes_written || bytes_written == -1)
 		return (0);
+	return (bytes_written);
 }
