06850a6af690262691175dd18fc272827df6f140
Amani <amanimavu@gmail.com>
2022-12-09 10:19:32 -0800

    correct documentation comments

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -4,7 +4,7 @@
  * dlistint_len - traverse the list and return the number of node
  * @h: pointer to the head node of list
  *
- * return: number of nodes
+ * Return: number of nodes
  */
 
 size_t dlistint_len(const dlistint_t *h)
