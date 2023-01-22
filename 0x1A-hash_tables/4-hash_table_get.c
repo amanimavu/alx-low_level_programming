4b70928e3c9e6127629386c57a87cfc492f86745
Amani Mavu <amanimavu@gmail.com>
2023-01-22 09:58:47 +0300

    make code compliant to betty

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,7 +12,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index, size;
 	hash_node_t *node;
-	char * value = NULL;
+	char *value = NULL;
 
 	if (!ht)
 		return (NULL);
